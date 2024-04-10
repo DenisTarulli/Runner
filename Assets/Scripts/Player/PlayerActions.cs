3b0fdf7310fb0f61b48c0b253772342794b6d94c
Denis Tarulli <132310271+DenisTarulli@users.noreply.github.com>
2024-04-10 15:47:37 -0300

    Tweaks

--- Assets/Scripts/Player/PlayerActions.cs
+++ Assets/Scripts/Player/PlayerActions.cs
@@ -9,8 +9,6 @@ public class PlayerActions : MonoBehaviour
     private bool canMove = true;
     public bool invulnerable = false;
 
-    [SerializeField] private Image image;
-
     private float movementCooldown = 0.08f;
     private float invulnerabilityTime = 2f;
     private const string MID_TO_LEFT = "MidtoLeft";
