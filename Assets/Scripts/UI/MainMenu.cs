6bcbeb31d85ede214c4c93cf179af257825a5f07
Denis Tarulli <132310271+DenisTarulli@users.noreply.github.com>
2024-04-16 11:11:06 -0300

    MainMenu

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -1,18 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //[SerializeField] private Texture2D defaultCursor;
+    //private Vector2 defaultHotspot = Vector2.zero;
+
+    private void Start()
     {
-        
+        //Cursor.SetCursor(defaultCursor, defaultHotspot, CursorMode.Auto);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void PlayGame()
     {
-        
+        SceneManager.LoadScene("GameScene");
+        //ClickSound();
+    }
+    /*
+    public void ClickSound()
+    {
+        AudioManager.instance.Play("ClickUI");
+    }
+
+    public void BackSound()
+    {
+        AudioManager.instance.Play("BackUI");
+    }
+    */
+    public void QuitGame()
+    {
+        Debug.Log("Quitting...");
+        Application.Quit();
     }
 }
